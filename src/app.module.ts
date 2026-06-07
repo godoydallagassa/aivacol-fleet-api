83f6816df4f48cbdffd6e5674a8b41dcff5042ee
godoydallagassa <homenotlet@gmail.com>
2026-06-07 19:57:10 -0300

    feat: integra modulos e validacao global

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,35 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { APP_GUARD } from '@nestjs/core';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuditModule } from './audit/audit.module';
+import { AuthModule } from './auth/auth.module';
+import { JwtAuthGuard } from './auth/jwt-auth.guard';
+import { RedisCacheModule } from './cache/redis-cache.module';
+import { databaseOptions } from './config/database-options';
+import { ModelsModule } from './models/models.module';
+import { UsersModule } from './users/users.module';
+import { VehiclesModule } from './vehicles/vehicles.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRootAsync({
+      useFactory: () => databaseOptions(),
+    }),
+    AuditModule,
+    RedisCacheModule,
+    UsersModule,
+    AuthModule,
+    ModelsModule,
+    VehiclesModule,
+  ],
+  controllers: [],
+  providers: [
+    {
+      provide: APP_GUARD,
+      useClass: JwtAuthGuard,
+    },
+  ],
 })
 export class AppModule {}
